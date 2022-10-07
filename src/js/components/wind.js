e3fc17e2e78b814e5e9b2a96c5fd00fc8fea1871
Caroline Akesson <ca223pw@student.lnu.se>
2022-10-07 16:39:07 -0500

    Create wind conversion component

---
+++ src/js/components/wind.js
@@ -0,0 +1,99 @@
+/**
+ * The wind web component module
+ *
+ */
+
+const template = document.createElement('template')
+template.innerHTML = `
+    <style>
+      #container {
+       display: flex;
+       flex-direction: column;
+       height: 80vh;
+       align-items: center;
+       justify-content: center;
+      }
+ 
+      h1 {
+       font-size: 2em;
+       color: #107dac;
+      }
+
+      input {
+        display: block;
+        margin: 10%;
+        padding: 5%;
+      }
+      
+      button {
+      background-color: #107dac;
+      color: white;
+      border: none;
+      margin: 10px;
+      padding: 10px;
+      border-radius: 10px; 
+    }
+
+    button:hover {
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      cursor: pointer;
+    }
+  
+    </style>
+    <div id="container">
+      <h1>Convert wind ...</h1>
+      <form action="">
+        <label for="input-from"></label>
+        <input list="wind-values" id="input-from" type="text" placeholder="From" />
+        <datalist id="wind-values">
+          <option value="Kilometer per hour">
+          <option value="Meter per Second">
+          <option value="Feet per second">
+          <option value="Miles per hour">
+          <option value="Knots">
+        </datalist>
+        <label for="input-to"></label>
+        <input list="wind-values" id="input-to" type="text" placeholder="To"/>
+        <datalist id="wind-values">
+          <option value="Kilometer per hour">
+          <option value="Meter per Second">
+          <option value="Feet per second">
+          <option value="Miles per hour">
+          <option value="Knots">
+        </datalist>
+        <button id="convert-btn">Convert!</button>
+      </form>
+    </div>
+  `
+
+customElements.define('wind-component',
+  /**
+   * Represents the navigation element.
+   */
+  class extends HTMLElement {
+    #inputFrom
+
+    #inputTo
+
+    #convertBtn
+
+    /**
+      * Creates an instance of the current type.
+      *
+      */
+    constructor() {
+      super()
+
+      // Attach a shadow DOM tree to this element and
+      // append the template to the shadow root.
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+
+      // Get necessary elements in shadowroot.
+      // REMOVE WHEN CREATED NEW ONES - this.inputField = this.shadowRoot.querySelector('#input-name')
+      this.inputFrom = this.shadowRoot.querySelector('#input-from')
+      this.inputTo = this.shadowRoot.querySelector('#input-to')
+      this.#convertBtn = this.shadowRoot.querySelector('#convert-btn')
+    }
+  }
+)
--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,13 @@
  */
  import './components/navigation.js'
  import './components/wizard.js'
+ import './components/wind.js'
 
  const container = document.querySelector('#container')
  const navigation = document.createElement('navigation-component')
- const wizard = document.createElement('wizard-component')
+ // const wizard = document.createElement('wizard-component')
+ const wind = document.createElement('wind-component')
+
 
  container.appendChild(navigation)
- container.appendChild(wizard)
+ container.appendChild(wind)
